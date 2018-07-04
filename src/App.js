03311e75ec9e3bdd4160944110b4616737aeb210
Zachary Lockner <zalockner@gmail.com>
2018-07-04 08:10:46 -0500

    added login/out button to navbar component and allowed pass thru of user state

--- src/App.js
+++ src/App.js
@@ -44,9 +44,17 @@ class App extends Component {
   }
 
   render() {
+    //need to use an if statement to check if user is null before digging through object, otherwise it will 
+    //kill the app with an error before it loads the user object
+    if (this.state.user != null) {  //need to user this.state here, can use this.props in components
+      console.log(this.state.user.email);  //remove this once we have figured out pushing state to a higher level.
+      console.log(this.state.user.photoURL);
+      console.log(this.state.user.displayName);
+    }
+
     return (
       <div className="App">
-        <Navbar user={this.state.user} />
+        <Navbar user={this.state.user} login={this.login.bind(this)} logout={this.logout.bind(this)} />
       </div>
     );
   }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 export class Navbar extends Component {
+	onLogout() {
+		this.props.logout();
+	}
+
+	onLogin() {
+		this.props.login();
+	}
+
 	render() {
-		console.log(this.props.user);  //remove this once we have figured out pushing state to a higher level.
+		//need to use an if statement to check if user is null before digging through object, otherwise it will 
+		//kill the app with an error before it loads the user object
+		if (this.props.user != null) {
+			console.log(this.props.user.email);  //remove this once we have figured out pushing state to a higher level.
+			console.log(this.props.user.photoURL);
+			console.log(this.props.user.displayName);
+		}
+
 		return (
 			<nav>
 				<div className="nav-wrapper blue darken-3">
@@ -12,9 +27,9 @@ export class Navbar extends Component {
 					</ul>
 					<div id="nav-mobile" className="right hide-on-med-and-down" style={{paddingRight:20}}>
 					    {this.props.user ?
-							<button onClick={this.logout}>Log Out</button>
+							<button onClick={this.onLogout.bind(this)}>Log Out</button>
 							:
-							<button onClick={this.login}>Log In</button>
+							<button onClick={this.onLogin.bind(this)}>Log In</button>
 						}
 					</div>
 				</div>
